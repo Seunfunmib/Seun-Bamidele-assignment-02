74511b71b6ddd69d372c2534ed629fde65e06569
Seunfunmi  Sheun <105411118+Seunfunmib@users.noreply.github.com>
2022-07-15 11:10:45 +0000

    I did the first two challenges by using the if and for loop statement.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;  
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+    sum = sum + arrayOfNumbers[i]
+   
+  }
+  return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+console.log(sumOfNumbers([1,2,3,4,5,6]))
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+function countEvenNumbers(arrayOfNumbers){
+  let even=[]
+  arrayOfNumbers.forEach(function(number){if
+    (number % 2 === 0){
+      even.push(number)}
+       })
+        return even.length;
+};
+  assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
